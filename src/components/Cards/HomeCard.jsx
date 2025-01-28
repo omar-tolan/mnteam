a430f12492f1a8ccffdb50063e6935f9f8c9a79e
omar tolan <omar.tolan@gmail.com>
2025-01-28 18:30:39 +0200

    modified homecard for medium screens

--- src/components/Cards/HomeCard.jsx
+++ src/components/Cards/HomeCard.jsx
@@ -3,7 +3,7 @@ import GuideButton from '../Buttons/GuideButton'
 
 function HomeCard() {
   return (
-    <div className='bg-white rounded-3xl flex flex-col justify-center items-center space-y-[17px] mx-[15px] mb-[15px] px-[15px] pb-[20px] pt-[30px] text-black'>
+    <div className='bg-white rounded-3xl flex flex-col justify-center items-center space-y-[17px] mx-[15px] mb-[15px] px-[15px] pb-[20px] pt-[30px] text-black md:w-[345px] md:h-[215px] md:m-[15px]'>
         <div>
            <p className='font-medium'>What do we offer?</p>
            <p className='font-light'>Get to know our services and how will you achieve your dream body.</p>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,9 +6,9 @@ function Homepage() {
   return (
     <div className='flex flex-col justify-center items-center'>
       <Navbar />
-    <div className='w-full min-h-screen bg-custom-image top-0 fixed flex flex-col justify-end items-center'> 
-      <HomeCard />
-    </div>
+      <div className='w-full min-h-screen bg-custom-image top-0 fixed flex flex-col justify-end items-center md:max-w-[calc(100%-1rem)] md:my-2 md:mx-2 md:rounded-4xl md:items-end'> 
+          <HomeCard />
+      </div>    
     </div>
   )
 }
