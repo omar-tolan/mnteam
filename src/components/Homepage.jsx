1ce58bde7e0975a83f64e52de5857d7a45e1a62a
omar tolan <omar.tolan@gmail.com>
2025-01-28 18:59:37 +0200

    added trainer introduction card

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
 import HomeCard from './Cards/HomeCard'
+import Trainer from './Cards/Trainer'
 
 function Homepage() {
   return (
     <div className='flex flex-col justify-center items-center'>
       <Navbar />
-      <div className='w-full min-h-screen bg-custom-image top-0 fixed flex flex-col justify-end items-center md:max-w-[calc(100%-1rem)] md:my-2 md:mx-2 md:rounded-4xl md:items-end'> 
+      <div className='w-full min-h-screen bg-custom-image flex flex-col justify-end items-center md:max-w-[99%] md:my-2 md:mx-2 md:rounded-4xl md:items-end'> 
           <HomeCard />
       </div>    
+      <Trainer />
     </div>
   )
 }
