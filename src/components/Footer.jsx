24fa0c64808c3860d9e2a45e980891819935f3a5
omar tolan <omar.tolan@gmail.com>
2025-01-30 13:35:02 +0200

    fixed services section bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 function Footer() {
   return (
     <div className='bg-black bottom-0 flex flex-col w-full mt-10'>
-        <div className='flex flex-row space-x-4 py-8'>
-            <img src='/logos/footerLogo.svg'/>
+        <div className='flex flex-row space-x-4 py-8 md:px-6 md:space-x-10'>
+            <img src='/logos/footerLogo.svg' className='md:w-[15vw]'/>
             <div className='flex flex-col'>
-                <p className='text-[20px]'>Contact Us</p>
-                <p className='text-light text-[14px]'>Cairo, Egypt</p>
-                <p className='text-light text-[14px]'>+20-012554786</p>
-                <p className='text-light text-[14px]'>info@mnteam.com</p>
+                <p className='text-[20px] md:text-[30px]'>Contact Us</p>
+                <p className='text-light text-[14px] md:text-[16px]'>Cairo, Egypt</p>
+                <p className='text-light text-[14px] md:text-[16px]'>+20-012554786</p>
+                <p className='text-light text-[14px] md:text-[16px]'>info@mnteam.com</p>
             </div><div className='flex flex-col'>
-                <p className='text-[20px]'>Connect</p>
+                <p className='text-[20px] md:text-[30px]'>Connect</p>
                 <div className='flex flex-row space-x-2'>
                     <img className='h-[20px]' src='/assets/icons8-facebook.svg'/>
                     <img className='h-[20px]' src='/assets/icons8-instagram.svg'/>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ function Services() {
   return (
     <div className='flex flex-col items-center w-full'>
         <p className='text-black text-[35px] text-center mb-4 mt-4 md:text-[50px] md:mb-10'>Our Services</p>
-        <div className='flex flex-col items-center space-y-2 md:flex-row md:items-start md:w-screen'>
+        <div className='flex flex-col items-center space-y-2 md:flex-row md:items-start md:w-full'>
             <CardOne />
             <CardTwo />
         </div>
