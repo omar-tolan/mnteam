dbe37556937a04ac6993b3352de4bf29f2efe500
omar tolan <omar.tolan@gmail.com>
2025-02-01 11:51:29 +0200

    fixed footer logo path

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='bg-black bottom-0 flex flex-col w-full mt-10'>
         <div className='flex flex-row space-x-4 py-8 md:px-6 md:space-x-10'>
-            <img src='/logos/footerLogo.svg' className='self-start md:w-[15vw]'/>
+            <img src='logos/footerLogo.svg' className='self-start md:w-[15vw]'/>
             <div className='flex flex-col text-right'>
                 <p className='text-[20px] md:text-[30px]'>اتصل بنا</p>
                 <p className='text-light text-[14px] md:text-[16px]'>القاهرة، مصر</p>
