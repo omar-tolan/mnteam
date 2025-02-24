4ae7082e7332e412798e74e891ed976809b53fd7
omar tolan <omar.tolan@gmail.com>
2025-02-24 10:33:20 +0200

    added navbar navigation functionality

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import Navbar from './Navbar'
-import HomeCard from './Cards/HomeCard'
-import Trainer from './Trainer'
-import Testimonials from './Testimonials'
-import Services from './Services'
-import Plans from './Plans'
-import Steps from './Steps'
-import Footer from './Footer'
+import React from "react";
+import Navbar from "./Navbar";
+import HomeCard from "./Cards/HomeCard";
+import Trainer from "./Trainer";
+import Testimonials from "./Testimonials";
+import Services from "./Services";
+import Plans from "./Plans";
+import Steps from "./Steps";
+import Footer from "./Footer";
+import { Element, Link } from "react-scroll";
 
 function Homepage() {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className="flex flex-col justify-center items-center">
       <Navbar />
-      <div className='w-full min-h-screen bg-custom-image flex flex-col justify-end items-center md:max-w-[98%] md:my-2 md:mx-2 md:rounded-4xl md:items-end'> 
-          <HomeCard />
-      </div>    
+      <div className="w-full min-h-screen bg-custom-image flex flex-col justify-end items-center md:max-w-[98%] md:my-2 md:mx-2 md:rounded-4xl md:items-end">
+        <HomeCard />
+      </div>
       <Trainer />
       <Testimonials />
-      <Services />
-      <Plans />
-      <Steps />
+      <Element name="services">
+        <Services />
+      </Element>
+      <Element name="plans">
+        <Plans />
+      </Element>
+      <Element name="cv">
+        <Steps />
+      </Element>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import NavButton from './Buttons/NavButton'
+import React from "react";
+import NavButton from "./Buttons/NavButton";
+import { animateScroll, Link } from "react-scroll";
 
 function Navbar() {
+  const scrollToTop = () => {
+    const options = {
+      duration: 500,
+      smooth: true,
+    };
+
+    animateScroll.scrollToTop(options);
+  };
   return (
-    <div className='bg-[rgba(55,55,55,0.5)] rounded-3xl h-[46px] mx-auto pr-[8px] pl-[21px] py-[6px] space-x-[20px] backdrop-blur-3xl flex flex-row justify-center items-center top-[30px] z-50 fixed'>
-      <img src='logos/navlogo.svg' alt='logo' className='h-[16px] w-[40px]'/>
-      <button className='text-[12px] font-bold hover:text-[#DB2E10]'>البرامج</button>
-      <button className='text-[12px] font-bold hover:text-[#DB2E10]'>اتصل بنا</button>
-      <NavButton />
+    <div className="bg-[rgba(55,55,55,0.5)] rounded-3xl h-[46px] mx-auto pr-[8px] pl-[21px] py-[6px] space-x-[20px] backdrop-blur-3xl flex flex-row justify-center items-center top-[30px] z-50 fixed">
+      <img
+        src="logos/navlogo.svg"
+        alt="logo"
+        className="h-[16px] w-[40px]"
+        onClick={scrollToTop}
+      />
+      <Link to="services" smooth={true} duration={500}>
+        <button className="text-[12px] font-bold hover:text-[#DB2E10]">
+          البرامج
+        </button>
+      </Link>
+      <Link to="cv" smooth={true} duration={500}>
+        <button className="text-[12px] font-bold hover:text-[#DB2E10]">
+          اتصل بنا
+        </button>
+      </Link>
+      <Link to="plans" smooth={true} duration={500}>
+        <NavButton />
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
